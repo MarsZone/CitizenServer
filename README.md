e9c5929535d1afdc5843fc4b2967b8f8b053b6e1
Mars <zm03322161@qq.com>
2020-08-14 18:01:23 +0800

    mybatis code,model,mapper

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # CitizenServer
 Server
 
+Base On [mall](https://github.com/macrozheng/mall "With a Title"). 
+
 This is the server for Citizen.
 Using Spring+Mariadb+redis...etc
---
+++ citizen-mbg/src/main/java/com/mars/citizen/mapper/SmsHomeNewProductMapper.java
@@ -0,0 +1,30 @@
+package com.mars.citizen.mapper;
+
+import com.mars.citizen.model.SmsHomeNewProduct;
+import com.mars.citizen.model.SmsHomeNewProductExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SmsHomeNewProductMapper {
+    long countByExample(SmsHomeNewProductExample example);
+
+    int deleteByExample(SmsHomeNewProductExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SmsHomeNewProduct record);
+
+    int insertSelective(SmsHomeNewProduct record);
+
+    List<SmsHomeNewProduct> selectByExample(SmsHomeNewProductExample example);
+
+    SmsHomeNewProduct selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") SmsHomeNewProduct record, @Param("example") SmsHomeNewProductExample example);
+
+    int updateByExample(@Param("record") SmsHomeNewProduct record, @Param("example") SmsHomeNewProductExample example);
+
+    int updateByPrimaryKeySelective(SmsHomeNewProduct record);
+
+    int updateByPrimaryKey(SmsHomeNewProduct record);
+}
